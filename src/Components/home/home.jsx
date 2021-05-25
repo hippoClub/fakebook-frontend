2cee970834308c2099fa6912d604d3b84a0309b6
Khalid Ibrahim <64450080+khaibr20@users.noreply.github.com>
2021-05-25 19:36:13 +0200

    fix: fetch all post and display

--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -1,34 +1,51 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./home.css"
 
 const Home = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+  const fetchPosts = async () => {
+    const result = await fetch("http://localhost:8080/api/post/allPost")
+    const response = await result.json()
+    setPosts(response)
+  }
+
   return (
     <div className="content_container">
       <div className="content">
         <h2 className="title">Feed</h2>
         {/* card */}
-        <div className="card">
-          <div className="card_avatar">
-            <img src="https://source.unsplash.com/user/erondu/40x40" alt="" />
-            <span>
-              <strong>Khalid Ibrahim &nbsp;</strong>
-            </span>
-            <span>
-              <small>-&nbsp; 24/05/2021</small>
-            </span>
-          </div>
-          <div className="postDescription">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Accusantium necessitatibus repellendus fugit sequi alias. Amet
-              maxime beatae recusandae ullam ipsam nam est unde repellendus
-              quidem totam, rerum aut corporis culpa.
-            </p>
-          </div>
-          <div className="imgPost">
-            <img src="https://source.unsplash.com/user/erondu/" alt="" />
-          </div>
-        </div>
+        {posts.map((post) => {
+          return (
+            <div className="card" key={post.postId}>
+              <div className="card_avatar">
+                <img
+                  src="https://source.unsplash.com/user/erondu/40x40"
+                  alt=""
+                />
+                <span>
+                  <strong>
+                    {post.firstname} {post.lastname} &nbsp;
+                  </strong>
+                </span>
+                <span>
+                  <small>-&nbsp; {post.createdAt}</small>
+                </span>
+              </div>
+              <div className="postDescription">
+                <p>{post.postDescription}</p>
+              </div>
+              <div className="imgPost">
+                <img src={post.image} alt="" />
+              </div>
+            </div>
+          )
+        })}
+        {/* end of card */}
       </div>
     </div>
   )
