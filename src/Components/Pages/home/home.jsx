5f5fb1f96cf97e494833e022391c027d01a9765f
Khalid Ibrahim <64450080+khaibr20@users.noreply.github.com>
2021-06-10 14:20:27 +0200

    feat: add loading page

--- src/Components/Pages/home/home.jsx
+++ src/Components/Pages/home/home.jsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react"
 import "./home.css"
 import Nav from "../../Controls/nav/nav"
+import MyLoader from "../../Controls/loading/loading"
 
 const Home = () => {
   const [posts, setPosts] = useState([])
   const [date, setDate] = useState("")
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     fetchPosts()
   }, [])
 
   const fetchPosts = async () => {
+    setLoading(true)
     const result = await fetch(
       "https://backend-hippo-club.herokuapp.com/api/post/allPost"
     )
     const response = await result.json()
+    setLoading(false)
     setPosts(response)
     response.map((res) => {
       const selectDate = res.createdAt
@@ -32,40 +36,44 @@ const Home = () => {
         <Nav></Nav>
       </div>
       <div className="content_container">
-        <div className="content">
+        <div>
           <h2 className="title">Feed</h2>
           {/* card */}
-          {posts.map((post) => {
-            return (
-              <div className="card" key={post.postId}>
-                <div className="card-header">
-                  <div className="card-header__avatar">
-                    <img
-                      src="https://source.unsplash.com/user/erondu/40x40"
-                      alt=""
-                    />
+          {loading ? (
+            <MyLoader />
+          ) : (
+            posts.map((post) => {
+              return (
+                <div className="card" key={post.postId}>
+                  <div className="card-header">
+                    <div className="card-header__avatar">
+                      <img
+                        src="https://source.unsplash.com/user/erondu/40x40"
+                        alt=""
+                      />
+                    </div>
+                    <div className="card-header__username">
+                      <span>
+                        <strong>
+                          {post.firstname} {post.lastname}
+                        </strong>
+                      </span>
+                      <br />
+                      <span>
+                        <small>{date}</small>
+                      </span>
+                    </div>
                   </div>
-                  <div className="card-header__username">
-                    <span>
-                      <strong>
-                        {post.firstname} {post.lastname}
-                      </strong>
-                    </span>
-                    <br />
-                    <span>
-                      <small>{date}</small>
-                    </span>
+                  <div className="card-header__postDescription">
+                    <p>{post.postDescription}</p>
+                  </div>
+                  <div className="card-header__imgPost">
+                    <img className="img" src={post.image} alt="" />
                   </div>
                 </div>
-                <div className="card-header__postDescription">
-                  <p>{post.postDescription}</p>
-                </div>
-                <div className="card-header__imgPost">
-                  <img className="img" src={post.image} alt="" />
-                </div>
-              </div>
-            )
-          })}
+              )
+            })
+          )}
           {/* end of card */}
         </div>
       </div>
--- src/Components/Pages/profile/profile.jsx
+++ src/Components/Pages/profile/profile.jsx
@@ -2,21 +2,25 @@ import React, { useState, useEffect } from "react"
 import "./profile.css"
 import PostItem from "./PostItem"
 import Nav from "../../Controls/nav/nav"
+import MyLoader from "../../Controls/loading/loading"
 
 const Profile = () => {
   const [date, setDate] = useState("")
   const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     fetchPosts()
   }, [])
   // fetch all user post
   const fetchPosts = async () => {
+    setLoading(true)
     const id = localStorage.getItem("id")
     const result = await fetch(
       `https://backend-hippo-club.herokuapp.com/api/post/userPosts/${id}`
     )
     const response = await result.json()
+    setLoading(false)
     setPosts(response)
     response.map((res) => {
       const selectDate = res.createdAt
@@ -34,15 +38,16 @@ const Profile = () => {
         <Nav />
       </div>
       <div className="content_container">
-        <div className="content">
+        <div>
           <h2 className="title">Profile</h2>
-          {posts.length !== 0 ? (
+          {loading ? (
+            <MyLoader />
+          ) : (
             posts.map((post) => (
               <PostItem postItem={post} date={date} key={post.postId} />
             ))
-          ) : (
-            <div>You don't have any post</div>
           )}
+          {posts.length === 0 && <div>You don't have any post</div>}
         </div>
       </div>
     </>
