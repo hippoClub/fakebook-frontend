031993c9ec044b686081231fcbafc1e9fd65f435
Khalid Ibrahim <64450080+khaibr20@users.noreply.github.com>
2021-06-10 15:32:27 +0200

    feat: add not found page

--- src/Components/Pages/pageNotFound/PageNotFound.jsx
+++ src/Components/Pages/pageNotFound/PageNotFound.jsx
@@ -1,10 +1,27 @@
 import React from "react"
+import Nav from "../../Controls/nav/nav"
+import Img from "./img.png"
+import "./pageNotFound.css"
 
 const PageNotFound = () => {
   return (
-    <div>
-      <h1>Page not found</h1>
-    </div>
+    <>
+      <div className="header">
+        <Nav />
+      </div>
+      <div className="content-container">
+        <div className="content">
+          <img
+            src={Img}
+            className="img_not_found"
+            alt="A man with a Space suite"
+            style={{ width: "50%" }}
+          />
+          <h1 style={{ color: "var(--primary-clr)" }}>404</h1>
+          <h3>Page not found</h3>
+        </div>
+      </div>
+    </>
   )
 }
 
